656b4f66c49baddefebaf25b99f5dcfb657f9d70
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-06-08 17:44:22 +0300

    remove contents of titleList

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 'use strict'
-
+{
 const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
@@ -25,16 +25,16 @@ const titleClickHandler = function(event){
         activeArticle.classList.remove('active');
     }
 
-  /* get 'href' attribute from the clicked link */
+  /*[DONE] get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute('href');
 
     console.log('articleSelector');
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /*[DONE] find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector);
 
     console.log(targetArticle);
-  /* add class 'active' to the correct article */
+  /*[DONE] add class 'active' to the correct article */
     targetArticle.classList.add('active');
 }
 
@@ -43,4 +43,32 @@ const links = document.querySelectorAll('.titles a');
   
     for(let link of links){
         link.addEventListener('click', titleClickHandler);
+}
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector)
+
+    document.querySelector('.titles').innerHTML = '';
+
+  /* for each article */
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
 }
